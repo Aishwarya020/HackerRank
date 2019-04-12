3cd639573b31cca1749feb2f9e3be890d6f4036a
Aishwarya020 <46655864+Aishwarya020@users.noreply.github.com>
2019-04-13 00:55:14 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,28 @@
+package hackerrank_practices;
+import java.util.*;
+
+public class Solution {
+	
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        
+        Double d=scan.nextDouble();
+        String str=scan.nextLine();
+        String s=scan.nextLine();
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+        scanner.close();
+		
+		
+			}
+
+}
